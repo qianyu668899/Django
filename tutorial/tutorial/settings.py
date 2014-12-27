281d66d825f7c3fcc1eedc62cd1d7551d49ef7e4
qianyu <jinggeqianyu1991@163.com>
2014-12-27 18:49:22 -0500

    update rest api

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 	'quickstart',
 	'rest_framework',
+	'snippets',
 )
 
 REST_FRAMEWORK = {
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -17,5 +17,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^', include(router.urls)),
-	url(r'^api_auth/', include('rest_framework.urls', namespace='rest_framework'))
+	url(r'^api_auth/', include('rest_framework.urls', namespace='rest_framework')),
+	url(r'^', include('snippets.urls')),
+	url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 )
